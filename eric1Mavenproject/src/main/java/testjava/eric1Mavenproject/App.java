114f1cf967ca64d4a36b9988ec407e0bf1987f7a
User <User@Dell-PC-Office>
2018-03-17 12:07:35 -0400

    1 try GIT

---
+++ eric1Mavenproject/src/main/java/testjava/eric1Mavenproject/App.java
@@ -0,0 +1,19 @@
+package testjava.eric1Mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( showStr() );
+    	System.out.println( "eric hello");
+    }
+    
+    static String  showStr()
+    {
+    	return "Hi Eric";
+    }
+}
