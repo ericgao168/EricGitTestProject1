2b522ac7206b2b178dd000f52e73c3783c6a6ce7
User <User@Dell-PC-Office>
2018-03-18 16:17:05 -0400

    March 18

--- eric1Mavenproject/src/main/java/testjava/eric1Mavenproject/App.java
+++ eric1Mavenproject/src/main/java/testjava/eric1Mavenproject/App.java
@@ -8,12 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( showStr() );
+        System.out.println( showStr() +  showStr1());
     	//System.out.println( "eric hello");
     }
     
     static String  showStr()
     {
-    	return "Hi Eric";
+    	return "Hi Eric ";
+    }
+    static String  showStr1()
+    {
+    	return "Hello world!! ";
     }
 }
